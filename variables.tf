755b08ba6a483e0f644f7c751878695bebb1993d
rburrs <richard.burrs@gmail.com>
2021-11-03 13:02:21 -0400

    formatted

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "loc" {
   description = "Default Azure region"
-  }
+}
 
 variable "services" {
-  type = list(string)
+  type        = list(string)
   description = "Services of service health events to monitor"
 }
 
 variable "events" {
-  type = list(string)
+  type        = list(string)
   description = "type of events to monitor"
 }
 
 variable "subscriptions" {
-  type = list(string)
+  type        = list(string)
   description = "subscriptions to monitor"
 }
  
