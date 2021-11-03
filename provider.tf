ff8d885c231b68cdba3a6cb8944e7e44e8ddf6d5
rburrs <richard.burrs@gmail.com>
2021-11-03 13:06:56 -0400

    provider

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
-  subscription_id = "c4991cab-5087-4f7e-b84f-0b71c321997f"
-  client_id       = "fbbe5d78-43f3-4353-ad83-1e7182e181ca"
-  client_secret   = "AIM7Q~lKLTB5e_LAmqW~eZbb3W0ePebnsiqOG"
-  tenant_id       = "9a4faa2a-9f67-4b91-8cf1-7545e55ce8a7"
-
-  features {}
+    features {}
 
 }
 
